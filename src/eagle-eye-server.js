3586e10c9654fafed10f563148c3b2bad871126d
Albin Hübsch <albin.hubsch@gmail.com>
2016-12-21 16:46:42 +0100

    Test add eagle event

--- src/eagle-eye-server.js
+++ src/eagle-eye-server.js
@@ -3,4 +3,19 @@
  * Author: Albin Hubsch <albin.hubsc@gmail.com>
  */
 
-// START ALL MODULES IN THIS FILE
+//
+import Eaglestore from "./modules/Eaglestore";
+
+const es = new Eaglestore();
+es.newEagleEvent();
+es.signIn();
+
+setTimeout(function () {
+    es.newEagleEvent("motion");
+}, 2000);
+
+setTimeout(function () {
+    es.signOut();
+}, 6000);
+
+// Try to sign in
